2ae78d980e6e53838470335f343ca2f33662c0a8
abasani21 <akbasani21@gmail.com>
2026-08-08 00:35:05 -0400

    Harden S3 infrastructure and isolate insecure test case

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,30 @@ provider "aws" {
 resource "aws_s3_bucket" "security_logs" {
   bucket = "akhil-secure-devsecops-logs-2026"
 }
+
+resource "aws_s3_bucket_public_access_block" "security_logs" {
+  bucket = aws_s3_bucket.security_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "security_logs" {
+  bucket = aws_s3_bucket.security_logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "security_logs" {
+  bucket = aws_s3_bucket.security_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
